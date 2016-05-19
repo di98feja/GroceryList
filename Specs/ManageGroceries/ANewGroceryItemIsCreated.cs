d1ac4c2c2272a99ed3aa5b9891eb15e6e51cddd3
JEF <JEF@VNSE-JEF>
2016-05-19 20:10:14 +0200

    First ManageGroceries specs

---
+++ Specs/ManageGroceries/ANewGroceryItemIsCreated.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Specs.ManageGroceries
+{
+	[Trait("New grocery is created", "")]
+	public class ANewGroceryItemIsCreated
+	{
+		[Fact(DisplayName = "An empty item is added to grocery items collection")]
+		public void AnEmptyItemIsAddedToGroceryItemsCollection()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact(DisplayName = "New item is automatically opened for edit")]
+		public void AppGoToEditItemScreen()
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
